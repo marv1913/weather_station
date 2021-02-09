81e363a403cf0f6caa7da9ac4829c17c123b1150
Marvin Rausch <marvin.rausch@live.de>
2021-02-09 18:13:30 +0100

    refactored of responses JSON format

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,44 +1,44 @@
 from flask import request
 from flask.json import jsonify
+from sqlalchemy import desc
 
 from model.db import WeatherDataModel, db, RainDataModel, PoolTemperatureModel
 from model.db import app
 
 QUERY_LIMIT = 144
+QUERY_LIMIT_RAIN = 24
+
 db.create_all()
+
 
 @app.route('/weather', methods=['GET', 'POST'])
 def handle_temperature():
     if request.method == 'GET':
-        weather_data = WeatherDataModel.query.order_by(WeatherDataModel.timestamp).limit(QUERY_LIMIT).all()
-        results = {'temperatures': [{"temperature": str(data.temperature), 'timestamp': str(data.timestamp)}
-                  "humidity": str(data.humidity)
-                  }
-             } for data in weather_data]}
-        rain_data = RainDataModel.query.order_by(RainDataModel.timestamp).limit(QUERY_LIMIT).all()
-        rainfall = [{str(data.timestamp): {'sum_1': str(data.rainfall), 'sum_24': str(data.rainfall_day)}} for data in
-                    rain_data]
-        results['rain'] = rainfall
-        return results
-    elif request.method == 'POST':
-        data = request.get_json()
-        new_temperature = WeatherDataModel(data['temperature'], data['timestamp'])
+        weather_data = WeatherDataModel.query.order_by(desc(WeatherDataModel.timestamp)).limit(QUERY_LIMIT).all()
+        temperatures = [{"temperature": str(data.temperature), "timestamp": str(data.timestamp)} for data in
+                        weather_data]
+        humidity = [{"humidity": str(data.humidity), "timestamp": str(data.timestamp)} for data in weather_data]
+        rain_data = RainDataModel.query.order_by(RainDataModel.timestamp).limit(QUERY_LIMIT_RAIN).all()
+        rainfall = [{'timestamp': str(data.timestamp), 'sum_1': str(data.rainfall), 'sum_24': str(data.rainfall_day)}
+                    for data in rain_data]
+        humidity = sort_list_of_dicts_by_timestamp(humidity)
+        rainfall = sort_list_of_dicts_by_timestamp(rainfall)
+        temperatures = sort_list_of_dicts_by_timestamp(temperatures)
 
-        db.session.add(new_temperature)
-        db.session.commit()
-        return {"message": "new temperature added"}
+        weather_data = {"temperatures": temperatures, "humidity": humidity, "rain": rainfall}
+        return jsonify(weather_data)
 
 
 @app.route('/pool_temperature', methods=['GET', 'POST'])
 def handle_pool_temperature():
     if request.method == 'GET':
-        # ToDo fix bug with query limit
-        weather_data = PoolTemperatureModel.query.order_by(PoolTemperatureModel.timestamp).limit(QUERY_LIMIT).all()
+        weather_data = PoolTemperatureModel.query.order_by(desc(PoolTemperatureModel.timestamp)).limit(QUERY_LIMIT).all()
         results = [
             {"timestamp": str(data.timestamp),
              "temperature": str(data.temperature)
              }
             for data in weather_data]
+        results = sort_list_of_dicts_by_timestamp(results)
         return jsonify(results)
     elif request.method == 'POST':
         data = request.get_json()
@@ -55,6 +55,9 @@ def after_request(response):
     header['Access-Control-Allow-Origin'] = '*'
     return response
 
+
+def sort_list_of_dicts_by_timestamp(list_of_dicts):
+    return sorted(list_of_dicts, key=lambda k: k['timestamp'])
 
 
 if __name__ == '__main__':
